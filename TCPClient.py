7af338d172cee7f83921fe249dc2604d104e735c
smell-ya-later <marius@hambro.eu>
2021-02-22 16:03:40 +0100

    bruh

--- TCPClient.py
+++ TCPClient.py
@@ -4,20 +4,24 @@ from lib import Lib
 
 
 HOST = "10.0.0.1"
-PORT = 9001
+PORT = 9000
 BUFSIZE = 1000
 
 def main(argv):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
+        print("Connection established.")
         s.sendall(b'/home/ase/Desktop/NGK-exercise6/hello.txt\0')
         receiveFile("/home/ase/Desktop/NGK-exercise6/hello.txt",s)
         s.close()
+        print("Connection closed")
     
 def receiveFile(fileName,  conn):
     file = open(fileName,"w")
+    print("Writing file.")
     file.write(Lib.readTextTCP(conn))
     file.close()
+    print("File closed.")
 
 if __name__ == "__main__":
    main(sys.argv[1:])
