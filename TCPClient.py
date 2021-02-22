7435fd0c6858e63b916375230049ba3839252dea
smell-ya-later <marius@hambro.eu>
2021-02-22 15:39:23 +0100

    Add client

---
+++ TCPClient.py
@@ -0,0 +1,22 @@
+import sys
+import socket
+from lib import Lib
+
+
+HOST = "10.0.0.1"
+PORT = 9001
+BUFSIZE = 1000
+
+def main(argv):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(b'/home/ase/Desktop/hello.txt\0')
+        receiveFile("/home/ase/Desktop/hello.txt",s)
+    
+def receiveFile(fileName,  conn):
+    file = open(fileName,"w")
+    file.write(Lib.readTextTCP(conn))
+    file.close()
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
