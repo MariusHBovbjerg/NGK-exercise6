d2884fa6498ea50e864fe934bbd331b707a5559f
Smell-ya-later <marius@hambro.eu>
2021-03-24 22:58:55 +0100

    Fixed server

--- TCPServer.py
+++ TCPServer.py
@@ -10,19 +10,21 @@ def main(argv):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
+        print("Listen")
         while True:
             conn, addr = s.accept()
+            print("accepted")
             while conn:
                 print('Connected by', addr, ".")
                 
                 data = Lib.readTextTCP(conn)
                 filesize = Lib.check_File_Exists(data)
                 if (filesize):
-                    filesizeStr = str(filesize) + "\0"
+                    filesizeStr = str(filesize) + '\0'
                     conn.send(filesizeStr.encode())
                     sendFile(data,conn)
                 else:
-                    filesizeStr = str(filesize) + "\0"
+                    filesizeStr = str(filesize) + '\0'
                     conn.send(filesizeStr.encode())
                 conn.close()
                 conn = None
@@ -33,11 +35,9 @@ def sendFile(fileName, conn):
     TEXTBUF = file.read(BUFSIZE)
     print("Read:",len(TEXTBUF), ".")
     while(TEXTBUF):
-        #Lib.writeTextTCP(TEXTBUF,conn)
         conn.send(TEXTBUF.encode())
         TEXTBUF = file.read(BUFSIZE)
         print("Read:",len(TEXTBUF),".")
-    conn.send(b'\0')
     file.close
     print("File closed.")
 
